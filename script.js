9ddd7f977e1c89873d856b958254ac99d7f297e1
rebeccalim20 <s10207898@connect.np.edu.sg>
2021-02-16 12:23:41 +0800

    fix bugs for product page showing the products

--- script.js
+++ script.js
@@ -8,98 +8,118 @@
 
 
 // fetch doc product details 
+// $(document).ready(() => {
+//     getProduct();
+   
+//     e.preventDefault();
+//   });
 
-document.getElementById('product-details').addEventListener
-function getProduct() {
-  fetch('https://fakestoreapi.com/products')
-    .then((response) => response.json())
-    .then((data) => {
-      let output = ' '
-      // console.log(data);
-      data.forEach(function (product) {
-        output += `    
+// function getProduct() {
+//   fetch('https://fakestoreapi.com/products')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let output = ' '
+//       // console.log(data);
+      
+//       data.forEach(function (product) {
+//         output += `    
+//         <div class="card">
+//         <img class="card-img-top" src = " ${product.image}" alt="">
+//         <div class="card-body">
+//         <div class="card-footer bg-transparent border-success">
+//         <h5 class="card-title"> ${product.title}</h5>
+//         <p class="text-center">$ ${product.price}</p>
+//         <a onclick="productselected('${product.id}')" class="btn btn-dark" href="#">Details</a>
+
+
+//         </div>
+//         </div>
+//         </div>
+//         `;
+        
+//       });
+//       document.getElementById('product-details').innerHTML = output;
+//     })
+//   }
+  
+
+//   function productselected(id){
+//     sessionStorage.setItem('productId', id);
+//     window.location = 'product.html';
+//     return false;
+//   }
+
+//   document.getElementById('product-infos').addEventListener
+
+
+
+
+$(document).ready(() => {
+    getProducts();
+});
+
+
+function getProducts(){
+ 
+  axios.get('https://fakestoreapi.com/products')
+    .then((response) => {
+      console.log(response);
+      let products = response.data;
+      let output = ' ';
+
+      $.each(products, (index,products) => {
+        output += `
         <div class="card">
-        <img class="card-img-top" src = " ${product.image}" alt="">
+        <img class="card-img-top" src = " ${products.image}" alt="">
         <div class="card-body">
         <div class="card-footer bg-transparent border-success">
-        <h5 class="card-title"> ${product.title}</h5>
-        <p class="text-center">$ ${product.price}</p>
-        <a onclick="productselected('${product.id}')" class="btn btn-dark" href="#">Details</a>
-
+        <h5 class="card-title"> ${products.title}</h5>
+        <p class="text-center">$ ${products.price}</p>
+        <a onclick="productselected('${products.id}')" class="btn btn-dark" href="#">Details</a>
 
         </div>
         </div>
         </div>
         `;
-        
       });
-      document.getElementById('product-details').innerHTML = output;
+
+      $('#product-details').html(output);
     })
-  }
-  
-  getProduct();
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
-  function productselected(id){
-    sessionStorage.setItem('productId', id);
-    window.location = 'product.html';
-    return false;
-  }
+function productselected(id){
+  sessionStorage.setItem('productId', id);
+  window.location = 'product.html';
+  return false;
+}
 
-
-  // product details more infos
 function productinfos(){
-   let productId = sessionStorage.getItem('productId');
-   fetch('https://fakestoreapi.com/products/'+productId)
-    .then((response) => response.json())
-    .then((data) => {
-      let output = ' '
-       console.log(data);
-       data.forEach(function (product) {
-        output += `    
-        <div class="card">
-        <img class="card-img-top" src = " ${product.image}" alt="">
-        <div class="card-body">
-        <div class="card-footer bg-transparent border-success">
-        <h5 class="card-title"> ${product.title}</h5>
-        <p class="text-center">$ ${product.price}</p>
-        <a onclick="productselected('${product.id}')" class="btn btn-dark" href="#">Movie Details</a>
+  let productId = sessionStorage.getItem('productId');
 
-        </div>
-        </div>
-        </div>
-        `;
-        
-      });
-      document.getElementById('product-infos').innerHTML = output;
+  axios.get('https://fakestoreapi.com/products/'+productId)
+    .then((response) => {
+      console.log(response);
+      let product = response.data;
+
+      let output =`
+      <div class="card">
+      <img class="card-img-top" src = " ${product.image}" alt="">
+      <div class="card-body">
+      <div class="card-footer bg-transparent border-success">
+      <h5 class="card-title"> ${product.title}</h5>
+      <p class="text-center">$ ${product.price}</p>
+
+      </div>
+      </div>
+      </div>
+      `;
+
+      $('#product-info').html(output);
     })
-
-  }
-
-
-  // function productinfos() {
-  //   fetch('https://fakestoreapi.com/products')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       let output = ' '
-  //       // console.log(data);
-  //       data.forEach(function (product) {
-  //         output += `    
-  //         <div class="card">
-  //         <img class="card-img-top" src = " ${product.image}" alt="">
-  //         <div class="card-body">
-  //         <div class="card-footer bg-transparent border-success">
-  //         <h5 class="card-title"> ${product.title}</h5>
-  //         <p class="text-center">$ ${product.price}</p>
-  //         <a onclick="productselected('${product.id}')" class="btn btn-dark" href="#">Movie Details</a>
-  
-  
-  //         </div>
-  //         </div>
-  //         </div>
-  //         `;
-          
-  //       });
-  //       document.getElementById('product-details').innerHTML = output;
-  //     })
-  //   }
-    
+    .catch((err) => {
+      console.log(err);
+    });
+}
