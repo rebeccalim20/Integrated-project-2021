6aa8ad4ab43f2c2005d3418ec3f73e0171868b6e
rebeccalim20 <s10207898@connect.np.edu.sg>
2021-02-19 21:08:54 +0800

    payment page , done alignment and layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // homepage layout and product page layout for using get request
 $(document).ready(() => {
   getProducts();
+  
 });
 
 function getProducts() {
@@ -73,10 +74,10 @@ function productinfos() {
 
       <ul class="nav fixed-bottom nav-pills nav-justified">
 		  <li class="nav-item-items">
-	  	<a href="#"><img src="img/cart.png" alt="" style="width:50px; height:50px;"></a>
+	  	<a href="payment.html"><img src="img/cart.png" alt="" style="width:50px; height:50px;"></a>
 	  	</li>
 	  	<li class="nav-item-item">
-	  	<a style ="color: #000000; font-size:18px;" href="#">Buy Now</a>
+	  	<a style ="color: #000000; font-size:18px;" href="payment.html">Buy Now</a>
 	  	</li>
 	    </ul>
       <br>
@@ -95,3 +96,143 @@ function productinfos() {
       console.log(err);
     });
 }
+
+
+
+
+
+
+function getfirst() {
+  axios.get('https://fakestoreapi.com/products/1')
+    .then((response) => {
+      console.log(response);
+      let first = response.data;
+      let output = `
+
+    <div class="col d-flex justify-content-center">
+        <div class="content-card" style="width: 10rem;">
+            <h2>Checkout</h2>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height: 10rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style="margin-bottom: 0px;">Delivery Address</p>
+                <p class="text-left"> Name: XXXXXXX
+                    <br> block number :XXXX
+                    <br> Singapore : 123456
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                  <img class="card-img-top" style="height :150px;width:130px;" src = " ${first.image}" alt="">
+                    <div class="d-flex flex-column">
+                        <div class="p-2">
+                        <h5 class="card-title"> ${first.title}</h5>
+                        </div>
+                        <div class="p-2"> <p class="text-center">price:$${first.price}</p></div>
+                        <div class="p-2"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:3rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style=" color: rgb(22, 137, 245);  margin-bottom: 0px;"><b>Shipping Method</b></p>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card"
+            style="background-color:#FFF9F6; width: 30rem;height:5rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style=" margin-bottom: 0px;">Ninja Van
+                    <br>
+                    (receive in 1-2 days)
+                </p>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:3rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style=" color: #219F58;  margin-bottom: 0px;"><b>Remarks</b></p>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:3rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style=" margin-bottom: 0px;"><b>Total Order:</b></p>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:3rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style="margin-bottom: 0px;"><b>My Vouchers:</b></p>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:3rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style="margin-bottom: 0px;"><b>Payment Option</b></p>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 30rem;height:8rem;box-shadow: 6px 5px 7px rgba(126, 126, 126, 0.144);">
+            <div class="card-body">
+                <p class="text-left" style="  margin-bottom: 0px;">product subtotal : <br> Shipping subtotal <br>Total
+                </p>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <ul class="nav fixed-bottom nav-pills nav-justified">
+        <li class="nav-item-items">
+            <p style="font-size: 20px;margin-bottom: 0px;">$109.95</p>
+            <p style="font-size: 15px;margin-bottom: 0px;">Gst included where applicable</p>
+        </li>
+        <li class="nav-item-item">
+        <a href=#" style="color:black; font-size: 20px;margin-bottom: 0px;">Place Order</a>
+        </li>
+    </ul>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    
+
+      `;
+      $('#firsts').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getfirst();
